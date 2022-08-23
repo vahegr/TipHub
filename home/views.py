2d9daa37f5d74a16f3e10b6b02ddcde942434267
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-08-24 00:22:43 +0430

    home view created

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView, TemplateView, DetailView
 
-# Create your views here.
+
+class HomePage(TemplateView):
+    template_name = 'index.html'
