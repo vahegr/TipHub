e2dbf69dc0f2fd5ec7ca2c1adb23f856df8cb147
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-09-03 19:02:59 +0430

    new app adden in the settings

--- tipHub/settings.py
+++ tipHub/settings.py
@@ -26,6 +26,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #my apps
+
+    'home',
+    'about',
 ]
 
 MIDDLEWARE = [
