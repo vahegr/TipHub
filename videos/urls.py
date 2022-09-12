ac67b98b2d934e70107597fcf249519898b6a860
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-09-13 01:26:53 +0430

    video detail view created

--- videos/urls.py
+++ videos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import VideoList
+from .views import VideoList, VideoDetail
 
 app_name = 'videos'
 
 urlpatterns = [
-    path('', VideoList.as_view(), name='all videos')
+    path('', VideoList.as_view(), name='all videos'),
+    path('video_detail/<int:id>/<slug:slug>', VideoDetail.as_view(), name='video detail')
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import Video, Category
 
@@ -7,3 +7,16 @@ class VideoList(ListView):
     model = Video
     template_name = 'videos/all-videos.html'
     queryset = Video.objects.filter(allowing=True)
+
+
+class VideoDetail(DetailView):
+    template_name = 'videos/video-detail.html'
+
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        id = self.kwargs.get('id')
+        video = Video.objects.get(id=id, slug=slug)
+        ip_address = self.request.user.ip_address
+        if ip_address not in video.hits.all():
+            video.hits.add(ip_address)
+        return video
