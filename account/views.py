9914d00182cc5e6ced9a712dbe50e34114ac73c2
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-09-15 14:10:28 +0430

    email field edited

--- account/views.py
+++ account/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import LogInForm
 
-# Create your views here.
+
+class UserLogIn(View):
+    def get(self, request):
+        if request.user.is_authenticated is True:
+            return redirect('home:home')
+        form = LogInForm()
+        return render(request, 'account/login.html', context={'form': form})
+
+    def post(self, request):
+        if request.user.is_authenticated is True:
+            return redirect('home:home')
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['email'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('home:home')
+            else:
+                form.add_error('email', 'اطلاعات وارد شده اشتباه است')
+        else:
+            form.add_error('email', 'اطلاعات وارد شده اشتباه است')
+
+        return render(request, 'account/login.html', context={'form': form})
