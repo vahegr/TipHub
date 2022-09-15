5ad079ca09faf7708298042bf1b60f4d27ebf371
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-09-15 20:03:17 +0430

    added recent videos in the index page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView, DetailView
+from videos.models import Video
 
 
 class HomePage(TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['video_list'] = Video.objects.filter(allowing=True)[:6]
+        return context
