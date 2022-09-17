68d1e474716cc4ec18e47e524dcb872871302abd
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-09-18 01:42:56 +0430

    about model created

--- about/models.py
+++ about/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class About(models.Model):
+    title = models.CharField(max_length=150, verbose_name='عنوان')
+    text = models.TextField(max_length=700, verbose_name='متن')
+    allowing = models.BooleanField(default=False, verbose_name='مجاز')
+
+    class Meta:
+        verbose_name = 'درباره'
+        verbose_name_plural = 'درباره ما'
+
+    def __str__(self):
+        return f"{self.title} - {self.text[:30]} ..."
