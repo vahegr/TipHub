b351ed8d80022c6eacbe84174212e54f408d272d
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-09-18 01:54:12 +0430

    abouts added to the aboutus page

--- about/views.py
+++ about/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
+from .models import About
 
 
 class AboutUs(TemplateView):
     template_name = 'about/about-us.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['abouts'] = About.objects.filter(allowing=True)
+        return context
