6e682ea5195cfcf3a4e81f3283580ecd8d3e6ca0
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-09-18 02:01:15 +0430

    users added to the aboutus page

--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
 from .models import About
+from account.models import User
 
 
 class AboutUs(TemplateView):
@@ -9,4 +10,5 @@ class AboutUs(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['abouts'] = About.objects.filter(allowing=True)
+        context['users'] = User.objects.filter(is_admin=True)
         return context
