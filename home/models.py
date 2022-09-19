b75722e03502f4b4f07d49a5fd51f94e6916e873
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-09-19 22:40:41 +0430

    contact info model created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactInfo(models.Model):
+    location = models.CharField(max_length=200, verbose_name='آدرس')
+    support = models.CharField(max_length=15, verbose_name='پشتیبانی')
+    email = models.EmailField(max_length=250, verbose_name='آدرس ایمیل')
+    instagram = models.URLField(blank=True, verbose_name='آیدی اینستاگرام')
+    telegram = models.URLField(blank=True, verbose_name='آدرس تلگرام')
+    whatsapp = models.URLField(blank=True, verbose_name='آدرس واتساپ')
+    twitter = models.URLField(blank=True, verbose_name='آدرس تویتر')
+
+    class Meta:
+        verbose_name = 'اطلاعات تماس'
+        verbose_name_plural = 'اطلاعات تماس'
+
+    def __str__(self):
+        return self.email
