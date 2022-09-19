46b1c2175fcf2a70faafad303fae1c10bfe66890
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-09-19 22:45:23 +0430

    contact info added to context proceccors

--- context_processors/context_processors.py
+++ context_processors/context_processors.py
@@ -1,7 +1,12 @@
 from videos.models import Category
+from home.models import ContactInfo
 
 
 def categories(request):
     category = Category.objects.all()
+    contact_info = ContactInfo.objects.get(allowing=True)
 
-    return {'categories': category}
+    return {
+        'categories': category,
+        'contact_info': contact_info,
+    }
