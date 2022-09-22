12129c7cef9a99ee5a4e7678847087a218757f3a
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-09-23 02:58:21 +0330

    like function created for like system

---
+++ assets/js/like-system.js
@@ -0,0 +1,14 @@
+function like(id, slug){
+    var element = document.getElementById("like")
+    var count = document.getElementById("count")
+    $.get(`/videos/video_like/${id}/${slug}`).then(response =>{
+        if(response['response'] === "unliked"){
+            element.className = "fa fa-heart-o"
+            count.innerText = Number(count.innerText) - 1
+        }
+        else{
+            element.className = "fa fa-heart"
+            count.innerText = Number(count.innerText) + 1
+        }
+    })
+}
