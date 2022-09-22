d3740e26361c911b66d844cec8acd3a8693441bd
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-09-23 02:22:19 +0330

    like view url created

--- videos/urls.py
+++ videos/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, re_path
-from .views import VideoList, VideoDetail, CategoryDetail, SearchResult
+from . import views
 
 app_name = 'videos'
 
 urlpatterns = [
-    path('', VideoList.as_view(), name='all videos'),
-    re_path(r'video_detail/(?P<id>[0-9]+)/(?P<slug>[-\w]+)/', VideoDetail.as_view(), name='video detail'),
-    re_path(r'category_detail/(?P<id>[0-9]+)/(?P<slug>[-\w]+)/', CategoryDetail.as_view(), name='category detail'),
-    path("search_result/", SearchResult.as_view(), name='search result'),
+    path('', views.VideoList.as_view(), name='all videos'),
+    re_path(r'video_detail/(?P<id>[0-9]+)/(?P<slug>[-\w]+)/', views.video_detail, name='video detail'),
+    re_path(r'category_detail/(?P<id>[0-9]+)/(?P<slug>[-\w]+)/', views.CategoryDetail.as_view(), name='category detail'),
+    re_path(r'video_like/(?P<id>[0-9]+)/(?P<slug>[-\w]+)/', views.video_like, name='video like'),
+    path("search_result/", views.SearchResult.as_view(), name='search result'),
 ]
