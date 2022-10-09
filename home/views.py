81b7be3d57bc9712bbf61ecd20a111a8ccd322a1
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-10-09 15:08:51 +0330

    popular videos query added to the home page

--- home/views.py
+++ home/views.py
@@ -8,5 +8,6 @@ class HomePage(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['video_list'] = Video.objects.filter(allowing=True)[:6]
+        context['recent_videos'] = Video.objects.filter(allowing=True)[:6]
+        context['popular_videos'] = Video.objects.filter(allowing=True).order_by('hits',)[:6]
         return context
